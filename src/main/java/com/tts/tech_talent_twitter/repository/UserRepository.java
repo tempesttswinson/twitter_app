9da716cbe91d59139a3f787b349e9388d3277a60
tempesttswinson <tempestt.swinson@gmail.com>
2020-07-28 11:00:52 -0400

    added data.sql

--- src/main/java/com/tts/tech_talent_twitter/repository/UserRepository.java
+++ src/main/java/com/tts/tech_talent_twitter/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.tts.tech_talent_twitter.repository;
 
+import com.tts.tech_talent_twitter.model.User;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
