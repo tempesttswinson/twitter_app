611aaa9908ebfe807742e64b5fc1ddd92a6f8ba9
tempesttswinson <tempestt.swinson@gmail.com>
2020-07-28 09:45:14 -0400

    created both user and role classes inside the model package

---
+++ src/main/java/com/tts/tech_talent_twitter/model/User.java
@@ -0,0 +1,47 @@
+package com.tts.tech_talent_twitter.model;
+
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String email;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private int active;
+
+    @CreationTimestamp
+    private Date createdAt;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+}
