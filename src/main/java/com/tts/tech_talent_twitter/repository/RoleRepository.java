3e229c49a83ba94379c16f38329993c8d4e479e1
tempesttswinson <tempestt.swinson@gmail.com>
2020-07-28 10:27:20 -0400

    created repositiories for user and role

---
+++ src/main/java/com/tts/tech_talent_twitter/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.tts.tech_talent_twitter.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends CrudRepository<Role, Long> {
+    Role findByRole(String role);
+}
---
+++ src/main/java/com/tts/tech_talent_twitter/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.tts.tech_talent_twitter.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByUsername(String username);
+}
