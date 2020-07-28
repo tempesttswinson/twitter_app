2f359341905c4f31b32212b2a960cc1d0e9cceb3
tempesttswinson <tempestt.swinson@gmail.com>
2020-07-28 08:38:51 -0400

    created maven project for twitter app. Added dependencies and edited application.properties file

---
+++ src/main/java/com/tts/tech_talent_twitter/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.tts.tech_talent_twitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/tech_talent_twitter/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.tech_talent_twitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
