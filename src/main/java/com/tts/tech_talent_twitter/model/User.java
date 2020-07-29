5710493876a88b1642f570b54ef228ba498d44ee
tempesttswinson <tempestt.swinson@gmail.com>
2020-07-29 09:35:00 -0400

    created a navbar fragment and added the fragment to each of the html pages

--- src/main/java/com/tts/tech_talent_twitter/model/User.java
+++ src/main/java/com/tts/tech_talent_twitter/model/User.java
@@ -12,8 +12,12 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -31,11 +35,24 @@ public class User {
     @Column(name = "user_id")
     private Long id;
 
+    @Email(message = "Please provide a valid email")
+    @NotEmpty(message = "Please provide an email")
     private String email;
+
+    @Length(min = 3, message = "Your username must have at least 3 characters")
+    @Length(max = 15, message = "Your username cannot have more than 15 characters")
+    @Pattern(regexp = "[^\\s]+", message = "Your username cannot contain spaces")
     private String username;
+
+    @Length(min = 5, message = "Your password must have at least 5 characters")
     private String password;
+
+    @NotEmpty(message = "Please provide your first name")
     private String firstName;
+
+    @NotEmpty(message = "Please provide your last name")
     private String lastName;
+
     private int active;
 
     @CreationTimestamp
